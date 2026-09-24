9e438f11ce2724d4a1829e1d09d5c0b129761fff
Gordon Chen <gordonchen2014@gmail.com>
2026-09-24 00:41:49 -0400

    feat: create ddragon service methods

--- src/app/ddragon/ddragon-service.ts
+++ src/app/ddragon/ddragon-service.ts
@@ -1,54 +1,26 @@
-import { Injectable } from '@angular/core';
+import { inject, Service } from '@angular/core';
+import { ImagePathService } from '@/ddragon/image-path-service';
+import { DdragonMetadataService } from '@/ddragon/ddragon-metadata-service';
+import { map, Observable } from 'rxjs';
 
-const VERSION = '16.16.1';
-const LOCALE = 'en_US';
-const BASE = 'https://ddragon.leagueoflegends.com';
-
-interface ImageTypeConfig {
-  path: (id: string) => string;
-  extension?: string;
-}
-
-const IMAGE_CONFIG = {
-  championSquare: {
-    path: (championId: string) => `${BASE}/cdn/${VERSION}/img/champion/${championId}`,
-    extension: 'png',
-  },
-  championFull: {
-    path: (championId: string) => `champion/${championId}/splash`,
-    extension: 'png',
-  },
-  profileIcon: {
-    path: (profileId: string) => `${BASE}/cdn/${VERSION}/img/profileicon/${profileId}`,
-    extension: 'png',
-  },
-} as const satisfies Record<string, ImageTypeConfig>;
-
-interface JsonTypeConfig {
-  path: string | ((id: string) => string);
-}
-
-const JSON_CONFIG: Record<string, JsonTypeConfig> = {
-  champion: {
-    path: (championId: string) =>
-      `https://ddragon.leagueoflegends.com/cdn/${VERSION}/data/${LOCALE}/champion/${championId}.json`,
-  },
-};
-
-type ImageType = keyof typeof IMAGE_CONFIG;
-type JsonType = keyof typeof JSON_CONFIG;
-
-@Injectable({
-  providedIn: 'root',
-})
+@Service()
 export class DdragonService {
-  public getImageUrl(type: ImageType, id: string): string {
-    const { path, extension = 'png' } = IMAGE_CONFIG[type];
-    return `${path(id)}.${extension}`;
+  private readonly ddragonMetaDataService = inject(DdragonMetadataService);
+  private readonly imagePathService = inject(ImagePathService);
+
+  public getChampionSquare(championId: string): Observable<string> {
+    return this.imagePathService
+      .getChampionSquare(championId)
+      .pipe(map((path) => `${this.ddragonMetaDataService.cdnUrl}/${path}`));
   }
 
-  public getJson(type: JsonType, id: string): Object {
-    const { path } = JSON_CONFIG[type];
-    return `${BASE}/${typeof path === 'string' ? path : path(id)}`;
+  public getChampionSplash(championId: number): string {
+    return `${this.ddragonMetaDataService.cdnUrl}/${this.imagePathService.getChampionSplash(championId)}`;
+  }
+
+  public getRuneImageUrl(runeId: number): Observable<string> {
+    return this.imagePathService
+      .getRune(runeId)
+      .pipe(map((path) => `${this.ddragonMetaDataService.cdnUrl}/${path}`));
   }
 }
